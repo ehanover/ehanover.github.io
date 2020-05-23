f0b7dd86dd17832b509cac76add1f8094b5f77b9
Ethan Hanover <ephanover@gmail.com>
2020-05-23 14:45:12 -0400

    Updates

---
+++ precache-manifest.634c34a8006b176eedcd5c004589ac71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2739bb7e2729d10ef6edb30d17eddc3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e337d84749921b5a0ac4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "74aca9cac6d1e673a2af",
+    "url": "/static/js/2.708bf9e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.708bf9e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e337d84749921b5a0ac4",
+    "url": "/static/js/main.b6fde71c.chunk.js"
+  },
+  {
+    "revision": "39594ab7674c044c53cb",
+    "url": "/static/js/runtime-main.56065bac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
